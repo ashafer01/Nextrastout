ba66bee2278d469b2d9f2fd31befc796068e53c6
Alex Shafer <ashafer01@gmail.com>
2015-02-04 10:07:46 -0800

    Moving log query parsing to its own function

--- functions/log_where.php
+++ functions/log_where.php
@@ -5,7 +5,7 @@ log::trace('entered f::log_where()');
 $_ARGV[] = null;
 $_ARGV[] = null;
 $_ARGV[] = null;
-list($param_string, $no_nicks, $date_limit, $leading_and) = $_ARGV;
+list($params, $no_nicks, $date_limit, $leading_and) = $_ARGV;
 if ($no_nicks === null) {
 	$no_nicks = false;
 }
@@ -13,164 +13,50 @@ if ($leading_and === null) {
 	$leading_and = true;
 }
 
-$params = explode(' ', $param_string);
-$likes = array();
-$notlikes = array();
-$req_wordbound = array();
-$exc_wordbound = array();
-$before = array();
-$after = array();
-$req_nicks = array();
-$exc_nicks = array();
-$req_re = array();
-$exc_re = array();
-
-$current_var = 'likes';
-$current_index = 0;
-foreach ($params as $param) {
-	if ($param == null) {
-		continue;
-	}
-	$fc = substr($param, 0 ,1);
-	switch ($fc) {
-		case '+':
-			# new LIKE phrase
-			$current_var = 'likes';
-			$current_index = count($likes);
-
-			$likes[$current_index] = array(substr($param, 1));
-			break;
-		case '-':
-			# new NOT LIKE phrase
-			$current_var = 'notlikes';
-			$current_index = count($notlikes);
-
-			$notlikes[$current_index] = array(substr($param, 1));
-			break;
-		case '=':
-			# new required word bound phrase
-			$current_var = 'req_wordbound';
-			$current_index = count($req_wordbound);
-
-			$req_wordbound[$current_index] = array(substr($param, 1));
-			break;
-		case '~':
-			# new excluded word bound phrase
-			$current_var = 'exc_wordbound';
-			$current_index = count($exc_wordbound);
-
-			$exc_wordbound[$current_index] = array(substr($param, 1));
-			break;
-		case '@':
-			# new nick list
-			if ($no_nicks) {
-				log::info('Ignoring nicks param in log_where() because no_nicks=true');
-			}
-			$current_var = 'req_nicks';
-			$current_index = count($req_nicks);
-
-			$req_nicks[$current_index] = array(substr($param, 1));
-			break;
-		case '^':
-			# new exclude nick list
-			if ($no_nicks) {
-				log::info('Ignoring exclude nicks param in log_where() because no_nicks=true');
-			}
-			$current_var = 'exc_nicks';
-			$current_index = count($exc_nicks);
-
-			$exc_nicks[$current_index] = array(substr($param, 1));
-			break;
-		case '<':
-			# new before time
-			$current_var = 'before';
-			$current_index = 0;
-
-			$before[0] = array(substr($param, 1));
-			break;
-		case '>':
-			# new after time
-			$current_var = 'after';
-			$current_index = 0;
-
-			$after[0] = array(substr($param, 1));
-			break;
-		default:
-			$fsc = substr($param, 0, 2);
-			switch ($fsc) {
-				case 'B+':
-					# new required word bound phrase
-					$current_var = 'req_wordbound';
-					$current_index = count($req_wordbound);
-
-					$req_wordbound[$current_index] = array(substr($param, 2));
-					break;
-				case 'B-':
-					# new excluded word bound phrase
-					$current_var = 'exc_wordbound';
-					$current_index = count($exc_wordbound);
-
-					$exc_wordbound[$current_index] = array(substr($param, 2));
-					break;
-				case 'R+':
-					# new required regex
-					$current_var = 'req_re';
-					$current_index = count($req_re);
-
-					$req_re[$current_index] = array(substr($param, 2));
-					break;
-				case 'R-':
-					# new excluded regex
-					$current_var = 'exc_re';
-					$current_index = count($exc_re);
-
-					$exc_re[$current_index] = array(substr($param, 2));
-					break;
-				default:
-					# add word to current array
-					${$current_var}[$current_index][] = $param;
-					break;
-			}
-			break;
-	}
+if (is_string($params)) {
+	$p = f::parse_logquery($params);
+} elseif (is_object($params)) {
+	$p = $params;
+} else {
+	$p = f::parse_logquery("$params");
 }
 
 $conds = array();
 
-foreach ($likes as $phrasewords) {
+foreach ($p->likes as $phrasewords) {
 	$phrase = dbescape(implode(' ', $phrasewords));
 	$conds[] = "(message ILIKE '%$phrase%')";
 }
 
-foreach ($notlikes as $phrasewords) {
+foreach ($p->notlikes as $phrasewords) {
 	$phrase = dbescape(implode(' ', $phrasewords));
 	$conds[] = "(message NOT ILIKE '%$phrase%')";
 }
 
-foreach ($req_wordbound as $phrasewords) {
+foreach ($p->req_wordbound as $phrasewords) {
 	$phrase = dbescape(preg_quote(implode(' ', $phrasewords)));
 	$conds[] = "(message ~* '[[:<:]]{$phrase}[[:>:]]')";
 }
 
-foreach ($exc_wordbound as $phrasewords) {
+foreach ($p->exc_wordbound as $phrasewords) {
 	$phrase = dbescape(preg_quote(implode(' ', $phrasewords)));
 	$conds[] = "(message !~* '[[:<:]]{$phrase}[[:>:]])";
 }
 
-foreach ($req_re as $rewords) {
+foreach ($p->req_re as $rewords) {
 	$rewords = dbescape(implode(' ', $rewords));
 	$conds[] = "(message ~* '$rewords')";
 }
 
-foreach ($exc_re as $rewords) {
+foreach ($p->exc_re as $rewords) {
 	$rewords = dbescape(implode(' ', $rewords));
 	$conds[] = "(message !~* '$rewords')";
 }
 
 if (!$no_nicks) {
-	if (count($req_nicks) > 0) {
+	if (count($p->req_nicks) > 0) {
 		$nicks = array();
-		foreach ($req_nicks as $nickgrp) {
+		foreach ($p->req_nicks as $nickgrp) {
 			foreach ($nickgrp as $nickstr) {
 				$nicklist = explode(',', strtolower($nickstr));
 				$nicks = array_merge($nicks, $nicklist);
@@ -183,9 +69,9 @@ if (!$no_nicks) {
 		$conds[] = "nick IN ($nicks)";
 	}
 
-	if (count($exc_nicks) > 0) {
+	if (count($p->exc_nicks) > 0) {
 		$nicks = array();
-		foreach ($exc_nicks as $nickstr) {
+		foreach ($p->exc_nicks as $nickstr) {
 			$nicklist = explode(',', strtolower($nickstr));
 			$nicks = array_merge($nicks, $nicklist);
 		}
@@ -198,16 +84,16 @@ if (!$no_nicks) {
 }
 
 if ($date_limit === null) {
-	if (count($before) > 0) {
-		$timestr = pg_escape_string(ExtraServ::$db, implode(' ', $before[0]));
+	if (count($p->before) > 0) {
+		$timestr = pg_escape_string(ExtraServ::$db, implode(' ', $p->before[0]));
 		$uts = strtotime($timestr);
 		if ($uts !== false) {
 			$conds[] = "(uts < $uts)";
 		}
 	}
 
-	if (count($after) > 0) {
-		$timestr = pg_escape_string(ExtraServ::$db, implode(' ', $after[0]));
+	if (count($p->after) > 0) {
+		$timestr = pg_escape_string(ExtraServ::$db, implode(' ', $p->after[0]));
 		$uts = strtotime($timestr);
 		if ($uts !== false) {
 			$conds[] = "(uts > $uts)";
@@ -215,8 +101,8 @@ if ($date_limit === null) {
 	}
 } else {
 	$before_uts = null;
-	if (count($before) > 0) {
-		$timestr = dbescape(implode(' ', $before[0]));
+	if (count($p->before) > 0) {
+		$timestr = dbescape(implode(' ', $p->before[0]));
 		$uts = strtotime($timestr);
 		if ($uts !== false) {
 			$before_uts = $uts;
@@ -224,8 +110,8 @@ if ($date_limit === null) {
 	}
 	
 	$after_uts = null;
-	if (count($after) > 0) {
-		$timestr = dbescape(implode(' ', $after[0]));
+	if (count($p->after) > 0) {
+		$timestr = dbescape(implode(' ', $p->after[0]));
 		$uts = strtotime($timestr);
 		if ($uts !== false) {
 			$after_uts = $uts;
---
+++ functions/parse_logquery.php
@@ -0,0 +1,125 @@
+//<?php
+
+log::trace('Entered f::parse_logquery()');
+
+list($param_string) = $_ARGV;
+
+$params = explode(' ', $param_string);
+
+$ret = new stdClass;
+$ret->likes = array();
+$ret->notlikes = array();
+$ret->req_wordbound = array();
+$ret->exc_wordbound = array();
+$ret->before = array();
+$ret->after = array();
+$ret->req_nicks = array();
+$ret->exc_nicks = array();
+$ret->req_re = array();
+$ret->exc_re = array();
+
+$current_var = 'likes';
+$current_index = 0;
+foreach ($params as $param) {
+	if ($param == null) {
+		continue;
+	}
+	$fc = substr($param, 0 ,1);
+	switch ($fc) {
+		case '+':
+			# new LIKE phrase
+			$current_var = 'likes';
+			$current_index = count($ret->likes);
+
+			$ret->likes[$current_index] = array(substr($param, 1));
+			break;
+		case '-':
+			# new NOT LIKE phrase
+			$current_var = 'notlikes';
+			$current_index = count($ret->notlikes);
+
+			$ret->notlikes[$current_index] = array(substr($param, 1));
+			break;
+		case '=':
+			# new required word bound phrase
+			$current_var = 'req_wordbound';
+			$current_index = count($ret->req_wordbound);
+
+			$ret->req_wordbound[$current_index] = array(substr($param, 1));
+			break;
+		case '~':
+			# new excluded word bound phrase
+			$current_var = 'exc_wordbound';
+			$current_index = count($ret->exc_wordbound);
+
+			$ret->exc_wordbound[$current_index] = array(substr($param, 1));
+			break;
+		case '@':
+			# new nick list
+			$current_var = 'req_nicks';
+			$current_index = count($ret->req_nicks);
+
+			$ret->req_nicks[$current_index] = array(substr($param, 1));
+			break;
+		case '^':
+			# new exclude nick list
+			$current_var = 'exc_nicks';
+			$current_index = count($ret->exc_nicks);
+
+			$ret->exc_nicks[$current_index] = array(substr($param, 1));
+			break;
+		case '<':
+			# new before time
+			$current_var = 'before';
+			$current_index = 0;
+
+			$ret->before[0] = array(substr($param, 1));
+			break;
+		case '>':
+			# new after time
+			$current_var = 'after';
+			$current_index = 0;
+
+			$ret->after[0] = array(substr($param, 1));
+			break;
+		default:
+			$fsc = substr($param, 0, 2);
+			switch ($fsc) {
+				case 'B+':
+					# new required word bound phrase
+					$current_var = 'req_wordbound';
+					$current_index = count($ret->req_wordbound);
+
+					$ret->req_wordbound[$current_index] = array(substr($param, 2));
+					break;
+				case 'B-':
+					# new excluded word bound phrase
+					$current_var = 'exc_wordbound';
+					$current_index = count($ret->exc_wordbound);
+
+					$ret->exc_wordbound[$current_index] = array(substr($param, 2));
+					break;
+				case 'R+':
+					# new required regex
+					$current_var = 'req_re';
+					$current_index = count($ret->req_re);
+
+					$ret->req_re[$current_index] = array(substr($param, 2));
+					break;
+				case 'R-':
+					# new excluded regex
+					$current_var = 'exc_re';
+					$current_index = count($ret->exc_re);
+
+					$ret->exc_re[$current_index] = array(substr($param, 2));
+					break;
+				default:
+					# add word to current array
+					$ret->{$current_var}[$current_index][] = $param;
+					break;
+			}
+			break;
+	}
+}
+
+return $ret;
