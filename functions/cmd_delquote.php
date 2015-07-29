32e9dff3920d47d9df3dca81f57cef4dfee1a4c1
Alex Shafer <ashafer01@gmail.com>
2015-07-29 14:31:45 -0700

    Adding delquote command

---
+++ functions/cmd_delquote.php
@@ -0,0 +1,50 @@
+//<?php
+
+log::trace('entered f::delquote()');
+list($_CMD, $params, $_i) = $_ARGV;
+
+if (preg_match('/^(\d+)$/', $params) !== 1) {
+	$_i['handle']->say($_i['reply_to'], 'Please specify a number');
+	return f::FALSE;
+}
+
+$quote_id = dbescape($params);
+
+$query = "SELECT set_by FROM quotedb WHERE id=$quote_id";
+log::debug("getquote/delquote query >> $query");
+$q = pg_query(ExtraServ::$db, $query);
+if ($q === false) {
+	log::error("Query failed");
+	log::error(pg_last_error());
+	$say = 'Query failed';
+} elseif (pg_num_rows($q) == 0) {
+	log::debug('No results for getquote/delquote query');
+	$say = 'Quote not found';
+} else {
+	log::debug('getquote/delquote query ok');
+
+	$qr = pg_fetch_assoc($q);
+
+	if (strtolower($_i['hostmask']->nick) != $qr['set_by']) {
+		log::info('Nickname does not match set_by for quote delete');
+		$say = "Only {$qr['set_by']} can delete quote #$quote_id";
+	} else {
+		$query = "DELETE FROM quotedb WHERE id=$quote_id";
+		log::debug("delquote query >> $query");
+		$q = pg_query(ExtraServ::$db, $query);
+
+		if ($q === false) {
+			log::error("Query failed");
+			log::error(pg_last_error());
+			$say = 'Failed to delete quote';
+		} elseif (pg_affected_rows($q) == 0) {
+			log::error('No affected rows after delete, but select found results!');
+			$say = 'No quote deleted';
+		} else {
+			log::debug('Delete OK');
+			$say = "Quote #$quote_id has been deleted";
+		}
+	}
+}
+
+$_i['handle']->say($_i['reply_to'], $say);
